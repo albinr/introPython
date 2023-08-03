3e3cecdb86464c0dc18a1971e11f1f53e34cdc9a
albinr <albin@rybergs.net>
2023-08-03 15:49:30 +0200

    done with contact manager

--- contact.py
+++
@@ -1,5 +0,0 @@
-print("create contact list")
-
-yesNo = input("Enter contact? y/n")
-
-
--- contactList.py
+++ contactList.py
@@ -1,20 +1,23 @@
 # main program
 def start():
-    contactList = createTestContacts()
+    # contactList = createTestContacts()
+    contactList = readContactsFromFile()
     print("Nu visar vi kontaktlistan vid start")
     showContactList(contactList)
     print("------------------------------------------")
 
     while True:
-        choose = input("n = new contact\nl = show list\nq = quit\n->").lower()
+        choose = input("n = new contact\nl = show list\nq = quit\n-> ").lower()
         print(choose)
         if choose == "n":
             createOneContact(contactList)
         elif choose == "l":
             showContactList(contactList)
         elif choose == "q":
-            endOfContactlist()
+            endOfContactList()
             exit()
+        else:
+            print("invalid option! Try again!")
     
    
 
@@ -28,13 +31,24 @@ def createTestContacts():
 
 def createOneContact(contactList):
     print("Input: Här matar vi in en contact och lägger in sist i contact list")
-
     contactName = input("Name: ")
     contactAge = input("Age: ")
     contactNumber = input("Number: ")
-    #Updaterar listan
-    contactList.append([contactName,contactAge,contactNumber])
+
+    # Update the list and the file
+    contactList.append([contactName, contactAge, contactNumber])
+    with open('contacts.txt', 'a') as file:
+        file.write(",".join([contactName, contactAge, contactNumber]) + "\n")
     return contactList
+
+def readContactsFromFile():
+    try:
+        with open('contacts.txt', 'r') as file:
+            lines = file.readlines()
+            contactList = [line.strip().split(",") for line in lines]
+            return contactList
+    except FileNotFoundError:
+        return []
 
 def showContactList(contactList):
     print("Nu visar vi kontaktlistan")
@@ -47,7 +61,7 @@ def showContactList(contactList):
 
 
 
-def endOfContactlist():
+def endOfContactList():
     print("Slut i rutan")
 
 
